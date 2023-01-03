8da3156160db208dbf82f7e08d81951f0b4f0500
Ismael Kouddane-Metz <ik-m@outlook.fr>
2023-01-03 17:06:41 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xf4eD25B90b1d0F8Cb0c8325dd8e825cE1eD80E5a";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
